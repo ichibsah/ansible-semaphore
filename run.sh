bfcddc8691235f3b5f873f90f1c10726b1dae8b2
adminqit <adminqit@2771-opsi-development>
2023-07-17 11:16:20 +0200

    Mon 17 Jul 2023 11:16:20 AM CEST

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #ansible-playbook -i hosts.ini win_update.yml -v
-ansible-playbook -i hosts.ini win_common_install.yml -v
+#ansible-playbook -i hosts.ini win_common_install.yml -v
+ansible-playbook -i hosts.ini win_restart.yml --check
