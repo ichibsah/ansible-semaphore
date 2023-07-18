fe352f2cf8614745c79e497fa22ec8e136a311bf
adminqit <adminqit@2771-opsi-development>
2023-07-18 16:43:55 +0200

    Tue 18 Jul 2023 04:43:55 PM CEST

---
+++ ansible-vault.sh
@@ -0,0 +1,2 @@
+ansible-vault edit ansible-vault.yml
+cp ansible-vault.yml ./group_vars/all/ansible-vault.yml
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+ansible-inventory -y --list
+
 #ansible-playbook -i hosts.ini win_update.yml -v --check
-ansible-playbook -i hosts.ini win_common_install.yml -v --check
+#ansible-playbook -i hosts.ini win_common_install.yml -v --check
+#ansible-playbook ansible_pull.yml -vvvv --check
+ansible-playbook ansible_pull.yml -vvvv 
 #ansible-playbook -i hosts.ini win_restart.yml --check
