35cb1f323f8645a2081fce405d87fd8d522b1321
adminqit <adminqit@2771-opsi-development>
2023-07-25 10:55:42 +0200

    Tue 25 Jul 2023 10:55:42 AM CEST

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 ansible-inventory -y --list
 
-#ansible-playbook -i hosts.ini win_update.yml
+ansible-playbook -i hosts.ini win_update.yml
+#ansible-playbook -i hosts.ini win_update.yml --check
 #ansible-playbook -i hosts.ini win_common_install.yml --check
 #ansible-playbook ansible_pull.yml -vvvv --check
-ansible-playbook run_users.yml -vvvv --check
+##ansible-playbook run_users.yml -vvvv --check
 #ansible-playbook ansible_pull.yml -vvvv 
 #ansible-playbook -i hosts.ini win_restart.yml --check
